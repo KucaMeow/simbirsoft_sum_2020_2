9d447b15269f9881db84c5b32abde29eba4e0d7c
KucaMeow <0tfuePEdDrbQ>
2020-07-10 16:54:06 +0300

    Project init commit

---
+++ storage_project/src/main/java/ru/stepan/ponomarev/storage_project/StorageProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.stepan.ponomarev.storage_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageProjectApplication.class, args);
+    }
+
+}
---
+++ storage_project/src/test/java/ru/stepan/ponomarev/storage_project/StorageProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stepan.ponomarev.storage_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
