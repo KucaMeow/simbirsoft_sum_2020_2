6576e4e28d7450f3900ad1c9d6d7252f6e3d84ac
KucaMeow <0tfuePEdDrbQ>
2020-08-18 17:22:23 +0300

    Added swagger-ui description

--- storage_project/src/main/java/ru/stepan/ponomarev/storage_project/controller/ReportController.java
+++ storage_project/src/main/java/ru/stepan/ponomarev/storage_project/controller/ReportController.java
@@ -1,6 +1,8 @@
 package ru.stepan.ponomarev.storage_project.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,9 @@ import ru.stepan.ponomarev.storage_project.service.PdfGenerator;
 import java.io.ByteArrayInputStream;
 import java.util.Date;
 
+/**
+ * Controller for getting reports in pdf format
+ */
 @RestController
 @RequestMapping("/pdf")
 public class ReportController {
@@ -24,6 +29,13 @@ public class ReportController {
         this.pdfGenerator = pdfGenerator;
     }
 
+    /**
+     * Getting report for all sold goods in current shop
+     * @param id id of shop
+     * @return APPLICATION_PDF_VALUE with generated PDF file
+     */
+    @ApiOperation(value = "Getting report for all sold goods in current shop",
+            produces = "APPLICATION_PDF_VALUE with generated PDF file")
     @GetMapping(value = "/sold/{id}",
             produces = MediaType.APPLICATION_PDF_VALUE)
     @PreAuthorize("isAuthenticated()")
@@ -41,6 +53,13 @@ public class ReportController {
                 .body(new InputStreamResource(bis));
     }
 
+    /**
+     * Getting report for average receipt in current shop
+     * @param id id of shop
+     * @return APPLICATION_PDF_VALUE with generated PDF file
+     */
+    @ApiOperation(value = "Getting report for average receipt in current shop",
+            produces = "APPLICATION_PDF_VALUE with generated PDF file")
     @GetMapping(value = "/average-receipt/{id}",
             produces = MediaType.APPLICATION_PDF_VALUE)
     @PreAuthorize("isAuthenticated()")
@@ -58,6 +77,13 @@ public class ReportController {
                 .body(new InputStreamResource(bis));
     }
 
+    /**
+     * Getting report for profit sum in current shop
+     * @param id id of shop
+     * @return APPLICATION_PDF_VALUE with generated PDF file
+     */
+    @ApiOperation(value = "Getting report for profit sum in current shop",
+            produces = "APPLICATION_PDF_VALUE with generated PDF file")
     @GetMapping(value = "/profit/{id}",
             produces = MediaType.APPLICATION_PDF_VALUE)
     @PreAuthorize("isAuthenticated()")
@@ -75,10 +101,20 @@ public class ReportController {
                 .body(new InputStreamResource(bis));
     }
 
+    /**
+     * Getting report for write-offs in current period of time
+     * @param start start date of time period
+     * @param end last date of time period
+     * @return APPLICATION_PDF_VALUE with generated PDF file
+     */
+    @ApiOperation(value = "Getting report for write-offs in current period of time",
+            produces = "APPLICATION_PDF_VALUE with generated PDF file",
+            notes = "Date format is yyyy-mm-dd; Example: \"pdf/write-off/2000-01-01/2000-01-31\"")
     @GetMapping(value = "/write-off/{start}/{end}",
             produces = MediaType.APPLICATION_PDF_VALUE)
     @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<InputStreamResource> writtenOffGoodsReport(@PathVariable Date start, @PathVariable Date end) {
+    public ResponseEntity<InputStreamResource> writtenOffGoodsReport(@PathVariable @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) Date start,
+                                                                     @PathVariable @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) Date end) {
 
         ByteArrayInputStream bis = pdfGenerator.writtenOffGoodsReport(start, end);
 
