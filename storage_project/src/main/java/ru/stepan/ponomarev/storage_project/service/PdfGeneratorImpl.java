b952dc529c66a99984bcbe455db10f34e258c2d8
KucaMeow <0tfuePEdDrbQ>
2020-08-19 14:13:08 +0300

    Fix: code repeating

--- storage_project/src/main/java/ru/stepan/ponomarev/storage_project/service/PdfGeneratorImpl.java
+++ storage_project/src/main/java/ru/stepan/ponomarev/storage_project/service/PdfGeneratorImpl.java
@@ -28,10 +28,42 @@ public class PdfGeneratorImpl implements PdfGenerator {
         this.shopRepository = shopRepository;
     }
 
+    private void addParagraph(String paragraph, Document document) throws DocumentException {
+        Font font = FontFactory.getFont(FontFactory.COURIER, 14, BaseColor.BLACK);
+        Paragraph para = new Paragraph( paragraph, font);
+        para.setAlignment(Element.ALIGN_CENTER);
+        document.add(para);
+        document.add(Chunk.NEWLINE);
+    }
+
+    private PdfPTable createTable(int numColumns, String[] headers) {
+        PdfPTable table = new PdfPTable(numColumns);
+
+        Stream.of(headers)
+                .forEach(headerTitle -> {
+                    PdfPCell header = new PdfPCell();
+                    Font headFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+                    header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+                    header.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    header.setBorderWidth(2);
+                    header.setPhrase(new Phrase(headerTitle, headFont));
+                    table.addCell(header);
+                });
+
+        return table;
+    }
+
+    private void addCellToTableWithText(String text, PdfPTable table) {
+        PdfPCell cell = new PdfPCell(new Phrase(text));
+        cell.setPaddingLeft(4);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(cell);
+    }
+
     @Override
     public ByteArrayInputStream soldGoodsReport(long shopId) {
         Optional<Shop> shop = shopRepository.findById(shopId);
-
         if(!shop.isPresent()) {
             throw new IllegalArgumentException("Can't find shop with such id");
         }
@@ -44,67 +76,17 @@ public class PdfGeneratorImpl implements PdfGenerator {
             PdfWriter.getInstance(document, out);
             document.open();
 
-            Font font = FontFactory.getFont(FontFactory.COURIER, 14, BaseColor.BLACK);
-            Paragraph para = new Paragraph( "Sold report for Shop: " + shop.get().getName(), font);
-            para.setAlignment(Element.ALIGN_CENTER);
-            document.add(para);
-            document.add(Chunk.NEWLINE);
-
-            PdfPTable table = new PdfPTable(2);
-            Stream.of("Transaction ID", "Transaction info")
-                    .forEach(headerTitle -> {
-                        PdfPCell header = new PdfPCell();
-                        Font headFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-                        header.setBackgroundColor(BaseColor.LIGHT_GRAY);
-                        header.setHorizontalAlignment(Element.ALIGN_CENTER);
-                        header.setBorderWidth(2);
-                        header.setPhrase(new Phrase(headerTitle, headFont));
-                        table.addCell(header);
-                    });
-
+            addParagraph("Sold report for Shop: " + shop.get().getName(), document);
+            PdfPTable table = createTable(2, new String[]{"Transaction ID", "Transaction info"});
             for (Transaction transaction : transactionList) {
-                PdfPCell idCell = new PdfPCell(new Phrase(String.valueOf(transaction.getId())));
-                idCell.setPaddingLeft(4);
-                idCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                idCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-                table.addCell(idCell);
-
-                PdfPTable transactionTable = new PdfPTable(2);
-                Stream.of("Product ID", "Product current cost")
-                        .forEach(a -> {
-                            PdfPCell header = new PdfPCell();
-                            Font headFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-                            header.setBackgroundColor(BaseColor.LIGHT_GRAY);
-                            header.setHorizontalAlignment(Element.ALIGN_CENTER);
-                            header.setBorderWidth(2);
-                            header.setPhrase(new Phrase(a, headFont));
-                            transactionTable.addCell(header);
-                        });
+                addCellToTableWithText(String.valueOf(transaction.getId()), table);
+                PdfPTable transactionTable = createTable(2, new String[]{"Product ID", "Product current cost"});
                 for (TransactionProductsInfo productsInfo : transaction.getProductList()) {
-                    PdfPCell productIdCell = new PdfPCell(new Phrase(String.valueOf(productsInfo.getProduct().getId())));
-                    productIdCell.setPaddingLeft(4);
-                    productIdCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                    productIdCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-                    transactionTable.addCell(productIdCell);
-
-                    PdfPCell productCostCell = new PdfPCell(new Phrase(String.valueOf(productsInfo.getProduct().getCost())));
-                    productCostCell.setPaddingLeft(4);
-                    productCostCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                    productCostCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-                    transactionTable.addCell(productCostCell);
+                    addCellToTableWithText(String.valueOf(productsInfo.getProduct().getId()), transactionTable);
+                    addCellToTableWithText(String.valueOf(productsInfo.getProduct().getCost()), transactionTable);
                 }
-
-                PdfPCell productSumLabel = new PdfPCell(new Phrase("Summary cost of transaction"));
-                productSumLabel.setPaddingLeft(4);
-                productSumLabel.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                productSumLabel.setHorizontalAlignment(Element.ALIGN_CENTER);
-                transactionTable.addCell(productSumLabel);
-
-                PdfPCell productSum = new PdfPCell(new Phrase(String.valueOf(transaction.getCost_sum())));
-                productSum.setPaddingLeft(4);
-                productSum.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                productSum.setHorizontalAlignment(Element.ALIGN_CENTER);
-                transactionTable.addCell(productSum);
+                addCellToTableWithText("Summary cost of transaction", transactionTable);
+                addCellToTableWithText(String.valueOf(transaction.getCost_sum()), transactionTable);
 
                 table.addCell(transactionTable);
             }
@@ -128,67 +110,20 @@ public class PdfGeneratorImpl implements PdfGenerator {
             PdfWriter.getInstance(document, out);
             document.open();
 
-            Font font = FontFactory.getFont(FontFactory.COURIER, 14, BaseColor.BLACK);
-            Paragraph para = new Paragraph( "Write-offs for period from " + start.toString() + " to " + end.toString(), font);
-            para.setAlignment(Element.ALIGN_CENTER);
-            document.add(para);
-            document.add(Chunk.NEWLINE);
-
-            PdfPTable table = new PdfPTable(2);
-            Stream.of("Transaction ID", "Transaction info")
-                    .forEach(headerTitle -> {
-                        PdfPCell header = new PdfPCell();
-                        Font headFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-                        header.setBackgroundColor(BaseColor.LIGHT_GRAY);
-                        header.setHorizontalAlignment(Element.ALIGN_CENTER);
-                        header.setBorderWidth(2);
-                        header.setPhrase(new Phrase(headerTitle, headFont));
-                        table.addCell(header);
-                    });
+            addParagraph("Write-offs for period from " + start.toString() + " to " + end.toString(), document);
+            PdfPTable table = createTable(2, new String[]{"Transaction ID", "Transaction info"});
 
             for (Transaction transaction : transactionList) {
-                PdfPCell idCell = new PdfPCell(new Phrase(String.valueOf(transaction.getId())));
-                idCell.setPaddingLeft(4);
-                idCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                idCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-                table.addCell(idCell);
+                addCellToTableWithText(String.valueOf(transaction.getId()), table);
 
-                PdfPTable transactionTable = new PdfPTable(2);
-                Stream.of("Product ID", "Count")
-                        .forEach(a -> {
-                            PdfPCell header = new PdfPCell();
-                            Font headFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-                            header.setBackgroundColor(BaseColor.LIGHT_GRAY);
-                            header.setHorizontalAlignment(Element.ALIGN_CENTER);
-                            header.setBorderWidth(2);
-                            header.setPhrase(new Phrase(a, headFont));
-                            transactionTable.addCell(header);
-                        });
+                PdfPTable transactionTable = createTable(2, new String[]{"Product ID", "Count"});
                 for (TransactionProductsInfo productsInfo : transaction.getProductList()) {
-                    PdfPCell productIdCell = new PdfPCell(new Phrase(String.valueOf(productsInfo.getProduct().getId())));
-                    productIdCell.setPaddingLeft(4);
-                    productIdCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                    productIdCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-                    transactionTable.addCell(productIdCell);
-
-                    PdfPCell productCountCell = new PdfPCell(new Phrase(String.valueOf(productsInfo.getQuantity())));
-                    productCountCell.setPaddingLeft(4);
-                    productCountCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                    productCountCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-                    transactionTable.addCell(productCountCell);
+                    addCellToTableWithText(String.valueOf(productsInfo.getProduct().getId()), transactionTable);
+                    addCellToTableWithText(String.valueOf(productsInfo.getQuantity()), transactionTable);
                 }
 
-                PdfPCell productSumLabel = new PdfPCell(new Phrase("Summary cost of transaction"));
-                productSumLabel.setPaddingLeft(4);
-                productSumLabel.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                productSumLabel.setHorizontalAlignment(Element.ALIGN_CENTER);
-                transactionTable.addCell(productSumLabel);
-
-                PdfPCell productSum = new PdfPCell(new Phrase(String.valueOf(transaction.getCost_sum())));
-                productSum.setPaddingLeft(4);
-                productSum.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                productSum.setHorizontalAlignment(Element.ALIGN_CENTER);
-                transactionTable.addCell(productSum);
+                addCellToTableWithText("Summary cost of transaction", transactionTable);
+                addCellToTableWithText(String.valueOf(transaction.getCost_sum()), transactionTable);
 
                 table.addCell(transactionTable);
             }
@@ -217,45 +152,19 @@ public class PdfGeneratorImpl implements PdfGenerator {
             PdfWriter.getInstance(document, out);
             document.open();
 
-            Font font = FontFactory.getFont(FontFactory.COURIER, 14, BaseColor.BLACK);
-            Paragraph para = new Paragraph( "Average receipt in Shop: " + shop.get().getName(), font);
-            para.setAlignment(Element.ALIGN_CENTER);
-            document.add(para);
-            document.add(Chunk.NEWLINE);
+            addParagraph("Average receipt in Shop: " + shop.get().getName(), document);
 
-            PdfPTable table = new PdfPTable(2);
-            Stream.of("Transaction ID", "Transaction cost")
-                    .forEach(headerTitle -> {
-                        PdfPCell header = new PdfPCell();
-                        Font headFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-                        header.setBackgroundColor(BaseColor.LIGHT_GRAY);
-                        header.setHorizontalAlignment(Element.ALIGN_CENTER);
-                        header.setBorderWidth(2);
-                        header.setPhrase(new Phrase(headerTitle, headFont));
-                        table.addCell(header);
-                    });
+            PdfPTable table = createTable(2, new String[]{"Transaction ID", "Transaction cost"});
 
             for (Transaction transaction : transactionList) {
                 fullSum += transaction.getCost_sum();
-
-                PdfPCell idCell = new PdfPCell(new Phrase(String.valueOf(transaction.getId())));
-                idCell.setPaddingLeft(4);
-                idCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                idCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-                table.addCell(idCell);
-
-                PdfPCell productSum = new PdfPCell(new Phrase(String.valueOf(transaction.getCost_sum())));
-                productSum.setPaddingLeft(4);
-                productSum.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                productSum.setHorizontalAlignment(Element.ALIGN_CENTER);
-                table.addCell(productSum);
+                addCellToTableWithText(String.valueOf(transaction.getId()), table);
+                addCellToTableWithText(String.valueOf(transaction.getCost_sum()), table);
             }
             document.add(table);
             document.add(Chunk.NEWLINE);
 
-            Paragraph para2 = new Paragraph( "Average receipt in this Shop: " + fullSum/transactionList.size(), font);
-            para2.setAlignment(Element.ALIGN_CENTER);
-            document.add(para2);
+            addParagraph("Average receipt in this Shop: " + fullSum/transactionList.size(), document);
 
             document.close();
         }catch(DocumentException e) {
@@ -280,45 +189,19 @@ public class PdfGeneratorImpl implements PdfGenerator {
             PdfWriter.getInstance(document, out);
             document.open();
 
-            Font font = FontFactory.getFont(FontFactory.COURIER, 14, BaseColor.BLACK);
-            Paragraph para = new Paragraph( "Average receipt in Shop: " + shop.get().getName(), font);
-            para.setAlignment(Element.ALIGN_CENTER);
-            document.add(para);
-            document.add(Chunk.NEWLINE);
+            addParagraph("Profit in Shop: " + shop.get().getName(), document);
 
-            PdfPTable table = new PdfPTable(2);
-            Stream.of("Transaction ID", "Transaction cost")
-                    .forEach(headerTitle -> {
-                        PdfPCell header = new PdfPCell();
-                        Font headFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-                        header.setBackgroundColor(BaseColor.LIGHT_GRAY);
-                        header.setHorizontalAlignment(Element.ALIGN_CENTER);
-                        header.setBorderWidth(2);
-                        header.setPhrase(new Phrase(headerTitle, headFont));
-                        table.addCell(header);
-                    });
+            PdfPTable table = createTable(2, new String[]{"Transaction ID", "Transaction cost"});
 
             for (Transaction transaction : transactionList) {
                 fullSum += transaction.getCost_sum();
-
-                PdfPCell idCell = new PdfPCell(new Phrase(String.valueOf(transaction.getId())));
-                idCell.setPaddingLeft(4);
-                idCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                idCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-                table.addCell(idCell);
-
-                PdfPCell productSum = new PdfPCell(new Phrase(String.valueOf(transaction.getCost_sum())));
-                productSum.setPaddingLeft(4);
-                productSum.setVerticalAlignment(Element.ALIGN_MIDDLE);
-                productSum.setHorizontalAlignment(Element.ALIGN_CENTER);
-                table.addCell(productSum);
+                addCellToTableWithText(String.valueOf(transaction.getId()), table);
+                addCellToTableWithText(String.valueOf(transaction.getCost_sum()), table);
             }
             document.add(table);
             document.add(Chunk.NEWLINE);
 
-            Paragraph para2 = new Paragraph( "Sold sum cost in this Shop: " + fullSum, font);
-            para2.setAlignment(Element.ALIGN_CENTER);
-            document.add(para2);
+            addParagraph("Sold sum cost in this Shop: " + fullSum, document);
 
             document.close();
         }catch(DocumentException e) {
